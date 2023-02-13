07cf2898418842adfd62f331b25dd1b23a2af5d7
svADMIN <svyouth_admin@MacBook-Air-som-tillhor-svYOUTH.local>
2023-02-13 19:45:14 +0100

    Done

--- main.java
+++ main.java
@@ -3,8 +3,9 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello Netbeans 12.5");
+        System.out.println("hejdå");
         System.out.println("hej");
-       
+        System.out.println("hej");
     }
     
 }
