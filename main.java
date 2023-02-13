808073156d5f18ad719b10e8360c5685404f6ee5
svADMIN <svyouth_admin@MacBook-Air-som-tillhor-svYOUTH.local>
2023-02-13 19:19:01 +0100

    Har ändrat scriptet

--- main.java
+++ main.java
@@ -1,13 +1,9 @@
 
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
-        System.out.print("Hello Netbeans 12.5");
-        
+        System.out.println("Hello Netbeans 12.5");
+        System.out.println("hej");
        
     }
     
