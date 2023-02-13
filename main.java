602200430dbe2f655ee9bcabe48c7cb1931bb02e
svADMIN <svyouth_admin@MacBook-Air-som-tillhor-svYOUTH.local>
2023-02-13 18:51:41 +0100

    Har skrivit mitt script

---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.print("Hello Netbeans 12.5");
+        
+       
+    }
+    
+}
